e5516ca84f7dbc033da8a013443cfee3601fb77b
Jashanpreet Singh <jashan.pahwa007@gmail.com>
2016-02-18 10:49:26 -0500

    update

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /* Redirect browser */
 echo $_SERVER['SERVER_NAME'];
-header("Location: http://" . $_SERVER['SERVER_NAME'] . "demohc32/hc3-demo/overview.php");
+header("Location: http://" . $_SERVER['SERVER_NAME'] . "/demohc32/hc3-demo/overview.php");
  exit;
 ?>
