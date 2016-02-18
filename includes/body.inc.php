4a8b28667ca34fbb54d43eb6fbc57bb506b29e1a
Jashanpreet Singh <jashan.pahwa007@gmail.com>
2016-02-18 10:46:42 -0500

    update

--- includes/body.inc.php
+++ includes/body.inc.php
@@ -10,37 +10,37 @@
 
              // PAGE CONTENT -->
              echo'<div class="page-content">';
-             if ($request_URI == '/hc3-demo/overview.php') {
+             if ($request_URI == '/demohc32/hc3-demo/overview.php') {
                 include 'content.inc.php';
              }
-             elseif ($request_URI == '/hc3-demo/needs-assessment.php') {
+             elseif ($request_URI == '/demohc32/hc3-demo/needs-assessment.php') {
                  include 'content-needs-assesment.inc.php';
              }
-             elseif($request_URI == '/hc3-demo/program-evaluation.php?p=rheumatory&t=all')
+             elseif($request_URI == '/demohc32/hc3-demo/program-evaluation.php?p=rheumatory&t=all')
              {
                 include 'rheumatoiry/content-rheumatory.inc.php';
              }
-             elseif($request_URI == '/hc3-demo/program-evaluation.php?p=rheumatory&mod=2')
+             elseif($request_URI == '/demohc32/hc3-demo/program-evaluation.php?p=rheumatory&mod=2')
              {
                 include 'rheumatoiry/content-rheumatory-mod-2.inc.php';
              }
-             elseif($request_URI == '/hc3-demo/program-evaluation.php?p=rheumatory&mod=3')
+             elseif($request_URI == '/demohc32/hc3-demo/program-evaluation.php?p=rheumatory&mod=3')
              {
                 include 'rheumatoiry/content-rheumatory-mod-3.inc.php';
              }
-             elseif($request_URI == '/hc3-demo/program-evaluation.php?p=rheumatory&mod=4')
+             elseif($request_URI == '/demohc32/hc3-demo/program-evaluation.php?p=rheumatory&mod=4')
              {
                 include 'rheumatoiry/content-rheumatory-mod-4.inc.php';
              }
-             elseif($request_URI == '/hc3-demo/program-evaluation.php?p=rheumatory&mod=5')
+             elseif($request_URI == '/demohc32/hc3-demo/program-evaluation.php?p=rheumatory&mod=5')
              {
                 include 'rheumatoiry/content-rheumatory-mod-5.inc.php';
              }
-             elseif($request_URI == '/hc3-demo/program-evaluation.php?p=rheumatory&mod=6')
+             elseif($request_URI == '/demohc32/hc3-demo/program-evaluation.php?p=rheumatory&mod=6')
              {
                 include 'rheumatoiry/content-rheumatory-mod-6.inc.php';
              }
-             // elseif($request_URI == '/hc3-demo/program-evaluation.php?p=rheumatory&t=non-accr')
+             // elseif($request_URI == '/demohc32/hc3-demo/program-evaluation.php?p=rheumatory&t=non-accr')
              // {
              //    include 'content-rheumatory-non-accr.inc.php';
              // }
@@ -62,33 +62,33 @@
         <?php
         include 'javascript.inc.php' ;
 
-        if ($request_URI == '/hc3-demo/overview.php') {
+        if ($request_URI == '/demohc32/hc3-demo/overview.php') {
                 //include 'javascript.inc.php' ;
              }
-             elseif ($request_URI == '/hc3-demo/needs-assessment.php') {
+             elseif ($request_URI == '/demohc32/hc3-demo/needs-assessment.php') {
                 //include 'javascript.inc.php' ;
              }
-             elseif($request_URI == '/hc3-demo/program-evaluation.php?p=rheumatory&t=all')
+             elseif($request_URI == '/demohc32/hc3-demo/program-evaluation.php?p=rheumatory&t=all')
              {
                //include 'javascript.inc.php' ;
              }
-             elseif($request_URI == '/hc3-demo/program-evaluation.php?p=rheumatory&mod=2')
+             elseif($request_URI == '/demohc32/hc3-demo/program-evaluation.php?p=rheumatory&mod=2')
              {
                 echo '<script type="text/javascript" src="includes/rheumatoiry/js/content-rheumatory-mod-2.js"></script>' ;
              }
-             elseif($request_URI == '/hc3-demo/program-evaluation.php?p=rheumatory&mod=3')
+             elseif($request_URI == '/demohc32/hc3-demo/program-evaluation.php?p=rheumatory&mod=3')
              {
                 echo '<script type="text/javascript" src="includes/rheumatoiry/js/content-rheumatory-mod-3.js"></script>' ;
              }
-             elseif($request_URI == '/hc3-demo/program-evaluation.php?p=rheumatory&mod=4')
+             elseif($request_URI == '/demohc32/hc3-demo/program-evaluation.php?p=rheumatory&mod=4')
              {
                 echo '<script type="text/javascript" src="includes/rheumatoiry/js/content-rheumatory-mod-4.js"></script>' ;
              }
-             elseif($request_URI == '/hc3-demo/program-evaluation.php?p=rheumatory&mod=5')
+             elseif($request_URI == '/demohc32/hc3-demo/program-evaluation.php?p=rheumatory&mod=5')
              {
                 echo '<script type="text/javascript" src="includes/rheumatoiry/js/content-rheumatory-mod-5.js"></script>' ;
              }
-             elseif($request_URI == '/hc3-demo/program-evaluation.php?p=rheumatory&mod=6')
+             elseif($request_URI == '/demohc32/hc3-demo/program-evaluation.php?p=rheumatory&mod=6')
              {
                 echo '<script type="text/javascript" src="includes/rheumatoiry/js/content-rheumatory-mod-6.js"></script>' ;
              }
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 /* Redirect browser */
-header("Location: http://www.flexxia.com/hc3-demo/overview.php");
+echo $_SERVER['SERVER_NAME'];
+header("Location: http://" . $_SERVER['SERVER_NAME'] . "demohc32/hc3-demo/overview.php");
  exit;
 ?>
